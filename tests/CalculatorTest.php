247c78725e00ca8f5c1192b67f1bf0312098cee2
makhotkinaangelina <gelyamakh@gmail.com>
2024-12-02 16:05:56 +0300

    update CalculatorTest.php

--- tests/CalculatorTest.php
+++ tests/CalculatorTest.php
@@ -9,5 +9,6 @@ class CalculatorTest extends TestCase
         $calculator = new Calculator();
         $this->assertEquals(3, $calculator->add(1, 2));
         $this->assertEquals(0, $calculator->add(1, -1));
+        $this->assertEquals(5, $calculator->add(5, 0));
     }
 }
